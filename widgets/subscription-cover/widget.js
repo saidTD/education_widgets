8532a5809cef208eb02da7617d618147f3befb2b
essamdineTD <essamdine@tech-dynamics.ma>
2023-05-08 15:08:48 +0100

    button space

---
+++ widgets/subscription-cover/widget.js
@@ -0,0 +1,21 @@
+
+function subscription(url){
+   const form = document.getElementById('subscription-form');
+   form.addEventListener('submit',async (e) => {
+      e.preventDefault();
+      const formData = new FormData(form);
+      try{
+      const response = await fetch(url, {
+            method: "POST", 
+            headers: {
+               "Content-Type": "application/json",
+            },
+            body: JSON.stringify(formData),
+         })
+      } catch (error) {
+         console.log(error)
+      }
+   })
+}
+
+subscription(url);
