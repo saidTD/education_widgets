079ef1d2624b1683411e7aa32780b623593924a9
said <bensamdisaid@gmail.com>
2023-05-16 14:20:53 +0000

    +++

--- widgets/our-partners/widget.js
+++ widgets/our-partners/widget.js
@@ -1,25 +1,29 @@
-import Splide from '@splidejs/splide';
+{% if params.list_partenaires|length > 5 %}
 
-const splide = new Splide('.splide.our-partners', {
-    type: 'loop',
-    perPage: '1',
-    perMove: '1',
-    autoplay: 'play',
-    interval: '1000',
-    arrows: false,
-    pagination: false,
-    mediaQuery: 'min',
-    breakpoints: {
-        500: {
-            perPage: 2
-        },
-        800: {
-            perPage: 3
-        },
-        992: {
-            perPage: 4
+
+    import Splide from '@splidejs/splide';
+
+    const splide = new Splide('.splide.our-partners', {
+        type: 'loop',
+        perPage: '1',
+        perMove: '1',
+        autoplay: 'play',
+        interval: '1000',
+        arrows: false,
+        pagination: false,
+        mediaQuery: 'min',
+        breakpoints: {
+            500: {
+                perPage: 2
+            },
+            800: {
+                perPage: 3
+            },
+            992: {
+                perPage: 4
+            }
         }
-    }
-});
+    });
 
-splide.mount();
+    splide.mount();
+{% endif %}
