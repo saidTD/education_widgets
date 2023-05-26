e65029ed7ee28f3fa97731e834a6ec95c90c187d
essamdineTD <essamdine@tech-dynamics.ma>
2023-05-26 17:29:35 +0100

    create scope id

--- widgets/accordione/dev/script.js
+++ widgets/accordione/dev/script.js
@@ -1,8 +1,36 @@
-const openBars = document.querySelectorAll('.accordion__item .accordion__toggler-bar');
-openBars.forEach(bar => {
-    bar.addEventListener('click', () => {
-        bar.classList.toggle('show');
-        let programList = bar.nextElementSibling ;
+
+function generateId(length) {
+    const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+
+    let randomId = ''
+
+    for(let i = 0 ; i < length; i++ ) {
+        randomId += characters.charAt(Math.floor(Math.random() * characters.length))
+    }
+
+    return randomId;
+}
+
+
+function scopeAccordion (id) {
+    const accordionItem = document.querySelector('.accordion__item');
+
+    accordionItem.setAttribute('id', id);
+}
+
+
+
+
+function accordion(elementSelector) {
+    const element = document.querySelector(elementSelector);
+    const openBar = element.querySelector('.accordion__toggler-bar');
+    console.log(element)
+    console.log(openBar)
+
+
+    openBar.addEventListener('click', () => {
+        openBar.classList.toggle('show');
+        let programList = openBar.nextElementSibling ;
 
         if(programList.style.maxHeight){
             programList.style.maxHeight = null;
@@ -11,5 +39,10 @@ openBars.forEach(bar => {
         else {
             programList.style.maxHeight = programList.scrollHeight + 'px';
         }
-    })
-});
+    });
+}
+
+const id = generateId(8);
+
+scopeAccordion(id);
+accordion(`#${id}`);
---
+++ widgets/accordione/widget.js
@@ -0,0 +1,23 @@
+function generateId(length) {
+  const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+
+  let randomId = ''
+
+  for(let i = 0 ; i < length; i++ ) {
+      randomId += characters.charAt(Math.floor(Math.random() * characters.length))
+  }
+
+  return randomId;
+}
+
+
+function scopeAccordion (id) {
+  const accordionItem = document.querySelector('.accordion__item');
+
+  accordionItem.setAttribute('id', id);
+}
+
+const id = generateId(8);
+
+scopeAccordion(id);
+accordion(`#${id}`);
