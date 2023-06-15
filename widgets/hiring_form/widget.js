7b2006c18d856b7b6f84a922b719d6cffeba175f
essamdineTD <essamdine@tech-dynamics.ma>
2023-06-15 12:22:38 +0100

    add form Plugin

--- widgets/hiring_form/widget.js
+++ widgets/hiring_form/widget.js
@@ -124,7 +124,17 @@
 //     }
 // })();
 
+(function openInputFile() {
+  const toggler = document.querySelector(".file-view");
+  const inputFile = document.getElementById("cv-file");
+  if (!toggler || !inputFile) {
+    return;
+  }
 
+  toggler.addEventListener("click", () => {
+    inputFile.click();
+  });
+})();
 
 new Form({ 
   elementSelctor: ".hiring form", 
