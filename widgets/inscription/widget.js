3d4d496ce20ddedc1aaf627ffc9144a29c5439d7
essamdineTD <essamdine@tech-dynamics.ma>
2023-06-15 11:51:10 +0100

    add form Plugin

--- widgets/inscription/widget.js
+++ widgets/inscription/widget.js
@@ -1,59 +1,61 @@
-(function() {
-  const form = document.querySelector('.inscription form');
-  const submitBtn = document.querySelector('.inscription form button');
+// (function() {
+//   const form = document.querySelector('.inscription form');
+//   const submitBtn = document.querySelector('.inscription form button');
 
-  submitBtn.addEventListener('click', (event) => {
-    event.preventDefault();
+//   submitBtn.addEventListener('click', (event) => {
+//     event.preventDefault();
 
-    if (form.checkValidity()) {
-      form.classList.remove('was-validated');
-      const formData = new FormData(form);
-      sendData([...formData]);
-    } else {
-      form.classList.add('was-validated');
-    }
-  });
+//     if (form.checkValidity()) {
+//       form.classList.remove('was-validated');
+//       const formData = new FormData(form);
+//       sendData([...formData]);
+//     } else {
+//       form.classList.add('was-validated');
+//     }
+//   });
 
-  function formatter(data) {
-    const formatedData = {};
-    data.forEach(([key, value]) => formatedData[key] = value);
-    return JSON.stringify(formatedData);
-  }
+//   function formatter(data) {
+//     const formatedData = {};
+//     data.forEach(([key, value]) => formatedData[key] = value);
+//     return JSON.stringify(formatedData);
+//   }
   
-  function sendData(formData) {
-    let formattedData = formatter(formData);
-    fetch("{{params.api_url}}", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: formattedData,
-    }).then((res) => {
-      if (res.ok) {
-        showMsg('votre formulaire a été soumis avec succès', 'success', form);
-        form.reset();
-      } else {
-        throw new Error("Error! status: " + res.status);
-      }
-    }).catch((err) => {
-      showMsg('Un problème est survenu lors de la soumission de votre formulaire, veuillez réessayer!', 'danger', form);
-    });
-  }
+//   function sendData(formData) {
+//     let formattedData = formatter(formData);
+//     fetch("{{params.api_url}}", {
+//       method: "POST",
+//       headers: {
+//         "Content-Type": "application/json",
+//       },
+//       body: formattedData,
+//     }).then((res) => {
+//       if (res.ok) {
+//         showMsg('votre formulaire a été soumis avec succès', 'success', form);
+//         form.reset();
+//       } else {
+//         throw new Error("Error! status: " + res.status);
+//       }
+//     }).catch((err) => {
+//       showMsg('Un problème est survenu lors de la soumission de votre formulaire, veuillez réessayer!', 'danger', form);
+//     });
+//   }
   
-  function showMsg(msg, type, wrapper) {
-    if (!msg || !type || !wrapper) return;
+//   function showMsg(msg, type, wrapper) {
+//     if (!msg || !type || !wrapper) return;
   
-    const id = new Date().getTime().toString();
-    const alert = document.createElement("div");
-    alert.classList.add('alert', `alert-${type}`);
-    alert.setAttribute('role', 'alert');
-    alert.setAttribute('id', id);
-    alert.textContent = msg;
-    wrapper.prepend(alert);
+//     const id = new Date().getTime().toString();
+//     const alert = document.createElement("div");
+//     alert.classList.add('alert', `alert-${type}`);
+//     alert.setAttribute('role', 'alert');
+//     alert.setAttribute('id', id);
+//     alert.textContent = msg;
+//     wrapper.prepend(alert);
   
-    const timeout = setTimeout(() => {
-      clearTimeout(timeout);
-      document.getElementById(id)?.remove();
-    }, 5000);
-  }
-})();
+//     const timeout = setTimeout(() => {
+//       clearTimeout(timeout);
+//       document.getElementById(id)?.remove();
+//     }, 5000);
+//   }
+// })();
+
+new Form({elementSelector : ".inscription form"})
